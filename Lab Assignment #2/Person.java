3f82c5d0ecf297875388ce4b48d00c229b0e198b
Hassaan Ahmad <hassaanahmad2005@gmail.com>
2025-03-25 14:51:24 +0500

    Create Person.java

---
+++ Lab Assignment #2/Person.java
@@ -0,0 +1,38 @@
+package school.Person;
+
+//Person class (parent class)
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 0) {
+            this.age = age;
+        } else {
+            System.out.println("Age must be positive!");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Age: " + age;
+    }
+}
+	
